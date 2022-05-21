9c4b16c90751952ae04dad255609b8fafd60469f
kkl4846 <kkl4846@cau.ac.kr>
2022-05-21 23:51:51 +0900

    [ADD] 유저 생성 API

--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -3,6 +3,11 @@ const message = {
   NOT_FOUND: "존재하지 않는 자원",
   BAD_REQUEST: "잘못된 요청",
   INTERNAL_SERVER_ERROR: "서버 내부 오류",
+
+
+  // user
+  CREATED_USER_SUCCESS: "유저 생성 성공",
+  
 };
 
 export default message;
