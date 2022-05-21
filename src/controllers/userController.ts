d7035f9aebd2228a3758be0117b6b033190eb9e5
kmebin <blowhuiee@gmail.com>
2022-05-22 04:06:19 +0900

    [UPDATE] Response Message 수정

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -45,7 +45,7 @@ const getUserDecisions = async (req: Request, res: Response) => {
     const data = await userService.getUserDecisions(userId);
 
     if (!data) {
-      return res.status(statusCode.NOT_FOUND).send(fail(statusCode.NOT_FOUND, message.READ_USER_QUESTIONS_FAIL));
+      return res.status(statusCode.NOT_FOUND).send(fail(statusCode.NOT_FOUND, message.NO_USER));
     }
     res.status(statusCode.OK).send(success(statusCode.OK, message.READ_USER_QUESTIONS_SUCCESS, data));
   } catch (error) {
